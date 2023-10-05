e1e3df728620390dd0829448e68816ff5b0960b8
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-10-05 14:28:32 +0000

    solved

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1 as math
+
+    a = 10
+    b = 5
+
+    print("{:d} + {:d} = {:d}".format(a, b, math.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, math.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, math.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, math.div(a, b)))
