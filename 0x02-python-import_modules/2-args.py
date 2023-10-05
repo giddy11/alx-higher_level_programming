1653a1be5e9be4177619e41fde4ddb63a8edfd91
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-10-05 15:38:08 +0000

    solved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,12 @@ import sys
 if __name__ == "__main__":
     num_of_args = len(sys.argv) - 1
 
-plural_suffix = "s" if num_of_args != 1 else ":"
-print("{} argument{}".format(num_of_args, plural_suffix))
+    if num_of_args == 0:
+        print("0 arguments.")
+    elif num_of_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_of_args))
 
-for i, arg in enumerate(sys.argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
